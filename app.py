b3b89269fab1f70694ea56acf9d03e8097b55175
rrangith <rrangith@edu.uwaterloo.ca>
2018-09-15 10:52:47 -0400

    removing db

--- app.py
+++ app.py
@@ -7,27 +7,27 @@ app = Flask(__name__)
 #db = SQLAlchemy(app)
 
 # Create database model
-class Location(db.Model):
-    __tablename__ = "locations"
-    id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.String(120))
-    longitude = db.Column(db.String(120))
-    heartbeats = db.Column(db.Integer)
+# class Location(db.Model):
+#     __tablename__ = "locations"
+#     id = db.Column(db.Integer, primary_key=True)
+#     latitude = db.Column(db.String(120))
+#     longitude = db.Column(db.String(120))
+#     heartbeats = db.Column(db.Integer)
 
-    def __init__(self, latitude, longitude, heartbeats):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.heartbeats = heartbeats
+#     def __init__(self, latitude, longitude, heartbeats):
+#         self.latitude = latitude
+#         self.longitude = longitude
+#         self.heartbeats = heartbeats
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+#     def __repr__(self):
+#         return '<id {}>'.format(self.id)
 
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'latitude': self.latitude,
-            'longitude': self.longitude,
-            'heartbeats':self.heartbeats
+#     def serialize(self):
+#         return {
+#             'id': self.id, 
+#             'latitude': self.latitude,
+#             'longitude': self.longitude,
+#             'heartbeats':self.heartbeats
         }
 
 @app.route('/')
