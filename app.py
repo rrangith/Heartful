165a418c09e430a5a46a0fb096bcca1984e7f792
rrangith <rrangith@edu.uwaterloo.ca>
2018-09-15 10:50:57 -0400

    removing db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, json, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
