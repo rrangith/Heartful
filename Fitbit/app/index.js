471b38f7661e1ff0c82155a09cd005a47792a3a7
zacharybys <zachary.bys@gmail.com>
2018-09-15 09:04:33 -0400

    Sending heartbeat to Companion and reading coordinates
    moving backend files to folder
    
    reverting changes
    
    adding psycopg to requirements
    
    testing variable endpoint
    
    testing without db
    
    removing db
    
    removing db
    
    typo
    
    typo

--- Fitbit/app/index.js
+++ Fitbit/app/index.js
@@ -1,13 +1,32 @@
 import document from "document";
 import { HeartRateSensor } from "heart-rate";
 import { geolocation } from "geolocation";
+import * as messaging from "messaging";
 
 let hrmData = document.getElementById("hrm-data");
-let locationData = document.getElementById("location-data");
 
 let hrm = new HeartRateSensor();
 
+var active= false;
+
 hrm.start();
+
+messaging.peerSocket.onopen = function() {
+  // Ready to send or receive messages
+  active = true;
+}
+
+function sendMessage(heartBeat) {
+  // Sample data
+  var data = {
+    beat: heartBeat
+  }
+
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send the data to peer as a message
+    messaging.peerSocket.send(data);
+  }
+}
 
 function refreshData() {
   
@@ -16,10 +35,14 @@ function refreshData() {
       heartRate: hrm.heartRate ? hrm.heartRate : 0
     }
   };
-
+  
+  if (active) {
+    sendMessage(data.hrm);
+  }
+  
   hrmData.text = JSON.stringify(data.hrm);
 }
 
 
 refreshData();
-setInterval(refreshData, 100);
+setInterval(refreshData, 20000);
--- Fitbit/companion/index.js
+++ Fitbit/companion/index.js
@@ -1,6 +1,25 @@
 import { peerSocket } from "messaging";
 import { geolocation } from "geolocation";
+import * as messaging from "messaging";
 
 geolocation.getCurrentPosition(function(position) {
    console.log(position.coords.latitude + ", " + position.coords.longitude);
 })
+
+// Listen for the onopen event
+messaging.peerSocket.onopen = function() {
+  // Ready to send or receive messages
+  messaging.peerSocket.onmessage = function(evt) {
+    // Output the message to the console
+    geolocation.getCurrentPosition(function(position) {
+      let data = {
+        info : {
+          heartrate: evt.data.beat.heartRate,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      };
+      console.log(JSON.stringify(data));
+    })
+  }
+}
