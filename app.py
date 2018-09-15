d080d10c264f11576e6e205588c0e07c3f18cae8
rrangith <rrangith@edu.uwaterloo.ca>
2018-09-15 10:49:10 -0400

    testing without db

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/locations'
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/locations'
+#db = SQLAlchemy(app)
 
 # Create database model
 class Location(db.Model):
