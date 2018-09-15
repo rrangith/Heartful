bcfa069b95b67b6e59dd7e0a853e65e5a813ed0d
zacharybys <zachary.bys@gmail.com>
2018-09-15 16:00:06 -0400

    Reading data points from the api and transforming them to heat map points

--- htn-frontend/src/components/MapContainer.js
+++ htn-frontend/src/components/MapContainer.js
@@ -2,26 +2,46 @@
 import {withGoogleMap, GoogleMap } from 'react-google-maps';
 import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer';
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const maps_key = process.env.REACT_APP_MAPS_KEY;
  
 class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    }; // <-- Initialize the state
+  }
+
+  componentWillMount() {
+    this.getInfo();
+  }
+
+  getInfo = () => {
+    axios.get(`https://boiling-inlet-47202.herokuapp.com/getall`)
+    .then(({ data }) => {
+        this.setState({
+          results: data.map(function(element){
+            return new google.maps.LatLng(element.latitude, element.longitude);
+          })                           
+        })
+        console.log(this.state.results);
+    })
+  }
+
   render() {
     const GoogleMapContainer = withGoogleMap(props => (
       <GoogleMap
         defaultCenter = {{
-          lat: 43.4728813,
-          lng: -80.5400242
+          lat: 43.472092,
+          lng: -80.542594
         }}
         defaultZoom = { 17 }
       >
+      <h1>KeepMeSafe</h1>
         <HeatmapLayer
-            data={[
-                new google.maps.LatLng(43.4728813, -80.5400242),
-                new google.maps.LatLng(43.4729813, -80.5400242),
-                new google.maps.LatLng(43.4727813, -80.5400242),
-                new google.maps.LatLng(43.4727313, -80.5400242)
-            ]}
+            data={this.state.results}
             options={{
               radius:35
             }}
