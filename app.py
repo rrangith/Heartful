9c50cddc36519ee7fb3c33ba427960672ee5b8ae
Rahul Rangith <rrangith@edu.uwaterloo.ca>
2018-09-15 20:27:17 -0400

    Check danger endpoint (#4)
    * typo
    
    * adding pandas to requirements
    
    * testing
    
    * test
    
    * test
    
    * fixing df reading
    
    * test
    
    * updating gitignore and fixing pandas json read
    
    * trying to fix json
    
    * casting to float

--- app.py
+++ app.py
@@ -56,14 +56,15 @@ def getById(id_):
 @app.route("/checkdanger/<latitude>/<longitude>")
 def checkDanger(latitude, longitude):
 	try:
-		df = pandas.DataFrame(getAll())
+		locations = Location.query.all()
+		df = pandas.DataFrame([l.serialize() for l in locations])
 		numClose = 0
 
 		for index, row in df.iterrows():
-			if math.sqrt((row['latitude'] - latitude) ** 2 + (row['longitude'] - longitude) ** 2) < 0.0005: #distance formula
-				numClose++
+			if math.sqrt((float(row['latitude']) - float(latitude)) ** 2 + (float(row['longitude']) - float(longitude)) ** 2) < 0.0005: #distance formula
+				numClose += 1
 
-		if numClose > 3:
+		if numClose > 3: #hardcoded to 3 for demo purposes
 			return jsonify("true") #dangerous
 		else:
 			return jsonify("false") #safe
