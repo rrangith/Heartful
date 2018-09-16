d26d4807380eca137b7229af768bc65b2fbc970d
rrangith <rrangith@edu.uwaterloo.ca>
2018-09-15 20:05:35 -0400

    test

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def getById(id_):
 def checkDanger(latitude, longitude):
 	try:
 		locations = Location.query.all()
+		return str(type(jsonify([l.serialize() for l in locations])))
 		df = pandas.read_json(jsonify([l.serialize() for l in locations]))
 		numClose = 0
 
